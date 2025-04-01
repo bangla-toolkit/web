16546281c37b9d785dadbfae4b5b4a3ca639edf3
Jubayer Al Mamun <jubayeramb@gmail.com>
2025-04-01 11:52:53 +0600

    feat: add basePath in next config & update GH action

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   serverExternalPackages: ["@electric-sql/pglite"],
   webpack: (config) => {
     config.module.rules.push({
